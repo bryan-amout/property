2b89a16a98bbae4fc0299aa2098ee617d7292d81
kelvinmuhuthia <kelvinmuhuthia@gmail.com>
2024-08-05 11:17:47 +0300

    initial commit

---
+++ app/src/main/java/com/example/propertyplus/ui/theme/screens/details/DetailsScreen.kt
@@ -0,0 +1,32 @@
+package com.example.propertyplus.ui.theme.screens.details
+
+import android.telecom.Call.Details
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.propertyplus.ui.theme.lightBlue
+import com.example.propertyplus.ui.theme.white
+
+@Composable
+fun DetailsScreen(navController: NavController){
+    Column (modifier = Modifier
+        .fillMaxSize()
+        .background(white),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){}
+
+
+}
+@Composable
+@Preview(showBackground = true)
+fun DetailsScreenPreview(){
+    DetailsScreen(rememberNavController())
+}
